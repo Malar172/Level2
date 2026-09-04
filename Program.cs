4cb1a27adbb28d9b6dddb70157028988e0d8da62
Malarvizhi Raju <malarkavin1913@gmail.com>
2026-09-04 15:37:29 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿List<string> Products = new List<string>();
+while (true)
+{
+    Console.WriteLine("Enter product names" +".");
+    Console.WriteLine("Type 'exit' to finish" +".");
+    Console.Write("Product:");
+
+    string product = Console.ReadLine().Trim();
+    if(product.ToLower() == "exit")
+    {
+        break;
+    }
+    
+    Products.Add(product);
+}
+
+ Products.Sort();
+
+Console.WriteLine("Sorted product list: ");
+
+foreach(string product in Products)
+{
+    Console.WriteLine("Product: " + product);
+}
+Console.ReadLine();
